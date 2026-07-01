129f40bbb118fc5574d8c07654a4a1938851c25a
Davaid.Ju <jupioneer8@gmail.com>
2026-07-01 08:18:18 +0800

    使用lombok注解精简代码

--- src/main/java/ju/pioneer/cloud_disk/entity/enums/ResponseCodeEnum.java
+++ src/main/java/ju/pioneer/cloud_disk/entity/enums/ResponseCodeEnum.java
@@ -7,7 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ResponseCodeEnum {
-    CODE_200(200, "请求成功"), CODE_404(404, "请求地址不存在"), CODE_600(600, "请求参数错误"), CODE_400(400, "请求参数错误"), CODE_601(601, "信息已存在"), CODE_500(500, "服务器返回错误，请联系管理员"), CODE_901(901, "登录过期，请重新登录");
+    CODE_200(200, "请求成功"),
+    CODE_404(404, "请求地址不存在"),
+    CODE_600(600, "请求参数错误"),
+    CODE_400(400, "请求参数错误"),
+    CODE_601(601, "信息已存在"),
+    CODE_500(500, "服务器返回错误，请联系管理员"),
+    CODE_901(901, "登录过期，请重新登录");
     private final int code;
     private final String msg;
 }
--- src/main/java/ju/pioneer/cloud_disk/entity/enums/UserStatuseEnum.java
+++ src/main/java/ju/pioneer/cloud_disk/entity/enums/UserStatuseEnum.java
@@ -1,19 +1,16 @@
 package ju.pioneer.cloud_disk.entity.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum UserStatuseEnum {
     ENABLE((byte) 1, "启用"),
     DISABLE((byte) 0, "禁用");
 
     private final byte status;
     private final String desc;
-
-    UserStatuseEnum(byte status, String desc) {
-        this.status = status;
-        this.desc = desc;
-    }
 
     public UserStatuseEnum getByStatus(byte status) {
         for (UserStatuseEnum value : values()) {
